550ae61c13b1cffafbb12258ccb929ea2d126e35
Ingo Wolf <me@ingowolf.au>
2025-12-27 20:08:02 +1100

    create route for next

---
+++ src/pages/next/[slug].ts
@@ -0,0 +1,16 @@
+import type { APIRoute } from "astro";
+import SITES from "../../../sites.ts";
+
+export const GET: APIRoute = async ({ redirect, params }) => {
+  const currentSite = SITES.findIndex((site) => site.slug === params.slug);
+  if (currentSite === -1) {
+    return redirect("/");
+  }
+
+  const newSiteIndex = currentSite + 1;
+  if (newSiteIndex >= SITES.length) {
+    redirect(SITES[0].url);
+  }
+
+  return redirect(SITES[newSiteIndex].url);
+};
