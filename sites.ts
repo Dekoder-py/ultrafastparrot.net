d28e47fabc4a799c84451d1f84ce26f629d5510c
Ingo Wolf <me@ingowolf.au>
2025-12-27 19:50:27 +1100

    add me :)

--- sites.ts
+++ sites.ts
@@ -3,4 +3,11 @@ export const SITES: {
   slug: string;
   url: string;
   email: string;
-}[] = [];
+}[] = [
+  {
+    name: "Ingo Wolf",
+    slug: "ingo",
+    url: "https://ingo.au",
+    email: "me@ingo.au",
+  },
+];
